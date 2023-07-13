d00f920352f7bc1392416e3e0bd14a6c53eec05d
atillakalay <atilla.kalayy@gmail.com>
2023-07-13 21:04:03 +0300

    Chapter 8 has been completed.

---
+++ Store/StoreApp/Components/CategoriesMenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+    public class CategoriesMenuViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public CategoriesMenuViewComponent(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categories = _serviceManager.CategoryService.GetAllCategories(false);
+            return View(categories);
+        }
+    }
+}
---
+++ Store/StoreApp/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+    public class ProductSummaryViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public ProductSummaryViewComponent(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        public string Invoke()
+        {
+            return _serviceManager.ProductService.GetAllProducts(false).Count().ToString();
+        }
+    }
+}
