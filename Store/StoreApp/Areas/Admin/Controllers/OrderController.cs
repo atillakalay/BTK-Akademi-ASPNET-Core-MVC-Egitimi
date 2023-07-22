6e07bf8fc3f184984f966b7b4e8648c34147a0a1
atillakalay <atilla.kalayy@gmail.com>
2023-07-22 18:11:24 +0300

    Chapter 13 has been complated.

--- Store/StoreApp/Areas/Admin/Controllers/OrderController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/OrderController.cs
@@ -18,6 +18,12 @@ namespace StoreApp.Areas.Admin.Controllers
             var orders = _serviceManager.OrderService.Orders;
             return View(orders);
         }
+        [HttpPost]
+        public IActionResult Complete([FromForm] int id)
+        {
+            _serviceManager.OrderService.Complete(id);
+            return RedirectToAction("Index");
+        }
 
 
     }
